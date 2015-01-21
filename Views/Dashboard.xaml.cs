fe18d2948876a74dfa4877dfd449243eafc299d5
unknown <ken.tung@RRSGPC81.readingroom.local>
2015-01-21 09:57:46 +0800

    Basic folder structure for MVVM

---
+++ Views/Dashboard.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace MCIFramework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                NorthwindContext context = new NorthwindContext();
+                context.FbPosts.OrderBy(c => c.ID).Load();
+                this.dataGrid.ItemsSource = context.FbPosts.Local;
+            }
+            catch (Exception ex)
+            {
+                var a = ex.Message;
+            }
+        }
+    }
+    
+    /// <summary>6
+    /// Entity framework context
+    /// </summary>
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<FacebookPost> FbPosts { get; set; }
+    }
+
+    /// <summary>
+    /// Employee class
+    /// </summary>
+    public class FacebookPost
+    {
+        public int ID { get; set; }
+        public int FacebookId { get; set; }
+        public string Post { get; set; }
+    }
+}
